27f9d897e6abf87b9bf994cb3a63ac7324a5a745
binio <tbiniecki@gmail.com>
2018-01-30 23:42:44 +0000

    get spring context

--- src/main/java/com/binio/didemo/Spring5DiApplication.java
+++ src/main/java/com/binio/didemo/Spring5DiApplication.java
@@ -1,12 +1,17 @@
 package com.binio.didemo;
 
+import com.binio.didemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring5DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5DiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(Spring5DiApplication.class);
+		MyController ctl = (MyController) ctx.getBean("myController");
+		ctl.hello();
+		//SpringApplication.run(Spring5DiApplication.class, args);
 	}
 }
