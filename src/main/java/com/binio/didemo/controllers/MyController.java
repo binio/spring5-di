8468869ac1ba3dc3f88828fa3d00acb789af1098
binio <tbiniecki@gmail.com>
2018-01-30 23:42:18 +0000

    add controller

---
+++ src/main/java/com/binio/didemo/controllers/MyController.java
@@ -0,0 +1,12 @@
+package com.binio.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello() {
+        System.out.println("hello world");
+        return "foo";
+    }
+}
