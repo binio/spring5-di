54c01b3bff60b34ffe1ed6111a31bc3486ea6dd8
binio <tbiniecki@gmail.com>
2018-01-30 11:33:47 +0000

    initial commit

---
+++ src/main/java/com/binio/didemo/Spring5DiApplication.java
@@ -0,0 +1,12 @@
+package com.binio.didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5DiApplication.class, args);
+	}
+}
