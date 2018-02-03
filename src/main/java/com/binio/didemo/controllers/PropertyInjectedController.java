dcd09b1bb3e80d20c37bbae8f8b4eb4b5abe5fb7
binio <tbiniecki@gmail.com>
2018-02-03 13:04:01 +0000

    controller

---
+++ src/main/java/com/binio/didemo/controllers/PropertyInjectedController.java
@@ -0,0 +1,16 @@
+package com.binio.didemo.controllers;
+
+import com.binio.didemo.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PropertyInjectedController {
+
+    @Autowired
+    public GreetingServiceImpl service;
+
+    public String sayHello() {
+        return service.sayHello();
+    }
+}
