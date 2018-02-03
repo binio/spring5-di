4f5b3e0c0696d762e4566a7523ac9a40c3075858
binio <tbiniecki@gmail.com>
2018-02-03 16:43:16 +0000

    add test

---
+++ src/test/java/com/binio/didemo/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,23 @@
+package com.binio.didemo.controllers;
+
+import com.binio.didemo.services.GreetingServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConstructorInjectedControllerTest {
+
+    ConstructorInjectedController controller;
+
+    @Before
+    public void setup(){
+        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    public void testSayHello() {
+        assertEquals("Hello GreetingServiceImpl", controller.sayHello());
+    }
+
+}
