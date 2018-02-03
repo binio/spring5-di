7b23b2af44528984583552e52a1aeb45fac73e90
binio <tbiniecki@gmail.com>
2018-02-03 16:43:02 +0000

    add test

--- src/main/java/com/binio/didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/com/binio/didemo/controllers/ConstructorInjectedController.java
@@ -1,4 +1,19 @@
 package com.binio.didemo.controllers;
 
+import com.binio.didemo.services.GreetingService;
+import com.binio.didemo.services.GreetingServiceImpl;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ConstructorInjectedController {
+
+    private GreetingService service;
+
+    public ConstructorInjectedController(GreetingService service) {
+        this.service = service;
+    }
+
+    public String sayHello() {
+        return service.sayHello();
+    }
 }
